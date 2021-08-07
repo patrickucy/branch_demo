45233b0a5814cfe282dfd928815f5fe910526a4c
patrickucy <patrick.ucy@gmail.com>
2021-08-07 16:34:27 +0800

    change to class base http input

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # branch_demo
 branch demo
+
+```sh
+fun config
+```
+config access and basic for alicloud
+
+```sh
+fun local invoke service
+```
+activate local project
+
+```sh
+fun deploy
+```
+deploy to cloud
+
--- index.py
+++ index.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 
-import logging
-HELLO_WORLD = b'Hello world!\n'
+HELLO_WORLD = b"Hello world!\n"
 
-# if you open the initializer feature, please implement the initializer function, as below:
-# def initializer(context):
-#    logger = logging.getLogger()  
-#    logger.info('initializing')
+
+class AppClass:
+    """Produce the same output, but using a class
+    """
+
+    def __init__(self, environ, start_response):
+        self.environ = environ
+        self.start = start_response
+
+    def __iter__(self):
+        status = '200 OK'
+        response_headers = [('Content-type', 'text/plain')]
+        self.start(status, response_headers)
+        yield HELLO_WORLD
 
 
 def handler(environ, start_response):
-    context = environ['fc.context']
-    request_uri = environ['fc.request_uri']
-    for k, v in environ.items():
-      if k.startswith('HTTP_'):
-        # process custom request headers
-        pass
-    # do something here
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    return [HELLO_WORLD]
+    return AppClass(environ, start_response)
